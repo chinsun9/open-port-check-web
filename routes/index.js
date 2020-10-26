5238ef0bf5963598c16b164399f73a2460990627
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-10-26 16:47:41 +0900

    refactor: console.log(ip)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(ip);
+  res.render('index', { title: 'Express', ip: ip });
 });
 
 module.exports = router;
