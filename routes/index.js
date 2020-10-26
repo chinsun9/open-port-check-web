231b93d94404a787ee62dd810feeab7c8e44a4ea
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-10-26 17:01:16 +0900

    feat: api

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(ip);
   res.render('index', { title: 'findip', ip: ip });
+});
+
+/* api */
+router.all('/api', function (req, res) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.json({ ip: ip });
 });
 
 module.exports = router;
